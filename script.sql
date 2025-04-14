0bec663fd6be4b18fce478447a978037ec7b9b6a
Renato <renatoaugustosa@gmail.com>
2025-04-14 19:51:11 -0300

    Criando model Produto.cs

--- script.sql
+++ script.sql
@@ -1,20 +1,31 @@
-CREATE DATABASE bdEcommerce;
+create	 database bdEcommerce;
 
-USE bdEcommerce;
+use bdEcommerce;
 
-CREATE TABLE Usuario(
+create table Usuario(
 	Id int primary key auto_increment,
     Nome varchar(50) not null,
     Email varchar(50) not null,
     Senha varchar(50) not null
 );
 
-CREATE TABLE Cliente(
+create table Cliente(
 	CodCli int primary key auto_increment,
     NomeCli varchar(50) not null,
     TelCli varchar(20) not null,
     EmailCli varchar(50) not null
-)
+);
 
-SELECT * FROM Usuario;
-SELECT * FROM Cliente;
+create table Produto(
+	CodProd int primary key auto_increment,
+    NomeProd varchar(50) not null,
+    DescricaoProd varchar(50) not null,
+    QuantidadeProd int,
+    PrecoProd float
+);
+
+select * from Usuario;
+select * from Cliente;
+select * from Produto;
+
+insert into Usuario(Nome, Email, Senha) values('admin', 'admin@email.com', '12345678');
